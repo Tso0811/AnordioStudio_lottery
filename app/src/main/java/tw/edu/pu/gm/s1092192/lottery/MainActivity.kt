24d90c7509df3281151516f821a15fb40c8575e6
Tso0811 <savagetso0811@gmail.com>
2024-03-06 20:19:57 +0800

    新增歸零及產生數字按鈕 並使其作用

--- app/src/main/java/tw/edu/pu/gm/s1092192/lottery/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/gm/s1092192/lottery/MainActivity.kt
@@ -2,6 +2,7 @@ package tw.edu.pu.gm.s1092192.lottery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var t:TextView=findViewById(R.id.txv)
         t.text = "0"
+    }
+
+    fun mainFun(v: View){
+        var txv: TextView = findViewById(R.id.txv)
+        if (v.id == R.id.btn_CreateNum){
+            txv.text = (1..100).random().toString()
+        }
+        else{
+            txv.text = ("0")
+        }
     }
 }
